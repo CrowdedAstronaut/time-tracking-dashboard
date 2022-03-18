bf9b3a0598c6df1b7a63a1f9a950212be0de3523
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-18 16:01:18 -0500

    Styling single card

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -3,13 +3,15 @@ import "../../css/App.css";
 export default function UserCard() {
   return (
     <>
-      <div className="card-information">
-        <div className="title">
-          <p>Work</p>
-          <p className="text-muted">···</p>
+      <div className="container">
+        <div className="card-information">
+          <div className="title">
+            <p>Work</p>
+            <p className="text-muted">···</p>
+          </div>
+          <h2 className="hours">32hrs</h2>
+          <p className="last-time">Last Week - 36hrs</p>
         </div>
-        <h2 className="hours">0hrs</h2>
-        <p className="last-time"> Select Time Tracking</p>
       </div>
     </>
   );
