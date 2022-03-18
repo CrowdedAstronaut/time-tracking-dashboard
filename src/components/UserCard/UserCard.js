c67dbc6d714cf30abd9b97eeefd39a5a94b72f90
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-18 15:12:06 -0500

    Created single card

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,3 +1,16 @@
+import "../../css/App.css";
+
 export default function UserCard() {
-  return <div>UserCard</div>;
+  return (
+    <>
+      <div className="card-information">
+        <div className="title">
+          <p>Work</p>
+          <p className="text-muted">···</p>
+        </div>
+        <h2 className="hours">0hrs</h2>
+        <p className="last-time"> Select Time Tracking</p>
+      </div>
+    </>
+  );
 }
