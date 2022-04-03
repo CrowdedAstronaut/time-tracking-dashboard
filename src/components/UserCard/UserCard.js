3a0023530dcb593eeb80d5f47cb9c693cff5221f
CrowdedAstronaut <ross@johnrossphillips.com>
2022-04-03 12:04:05 -0500

    Added handler functions for the 3 pieces of state to be updated

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,7 +1,29 @@
 import "../../css/App.css";
 import userimage from "../../assets/images/image-jeremy.png";
+import { useState } from "react";
 
 export default function UserCard() {
+  const [isTimeDaily, setTimeDaily] = useState(false);
+  const [isTimeWeekly, setTimeWeekly] = useState(true);
+  const [isMonthly, setTimeMonthly] = useState(false);
+
+  const handleClick = () => {
+    setTimeDaily(true);
+    setTimeWeekly(false);
+    setTimeMonthly(false);
+  };
+
+  const handleClickWeek = () => {
+    setTimeDaily(false);
+    setTimeWeekly(true);
+    setTimeMonthly(false);
+  };
+
+  const handleClickMonth = () => {
+    setTimeDaily(false);
+    setTimeMonthly(true);
+    setTimeWeekly(false);
+  };
   return (
     <>
       {/* <div className="user-container"> */}
@@ -21,15 +43,41 @@ export default function UserCard() {
         </div>
 
         <div className="time-option">
-          <button id="daily-btn" className="text-muted">
-            Daily
-          </button>
-          <button id="weekly-btn" className="text-muted">
-            Weekly
-          </button>
-          <button id="monthly-btn" className="text-muted">
-            Monthly
-          </button>
+          <ul className="trackingList">
+            <li className="trackingListItem">
+              <a
+                href="/#"
+                className={
+                  isTimeDaily ? "DailyColor" : "Daily"
+                }
+                onClick={handleClick}
+              >
+                Daily
+              </a>
+            </li>
+            <li className="trackingListItem">
+              <a
+                href="/#"
+                className={
+                  isTimeWeekly ? "WeeklyColor" : "Weekly"
+                }
+                onClick={handleClickWeek}
+              >
+                Weekly
+              </a>
+            </li>
+            <li className="trackingListItem">
+              <a
+                href="/#"
+                className={
+                  isMonthly ? "MonthlyColor" : "Monthly"
+                }
+                onClick={handleClickMonth}
+              >
+                Monthly
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
       {/* </div> */}
